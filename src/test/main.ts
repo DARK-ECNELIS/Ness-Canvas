5eeaba86e30c1b2503a86e7092505400c93ed110
DARK-ECNELIS <darkecnelis@gmail.com>
2023-03-11 17:12:32 -0400

    Test pre release

--- src/test/main.ts
+++ src/test/main.ts
@@ -1,29 +1,53 @@
-import { loadImage } from "canvas";
+import { gifVersion, loadImage } from "canvas";
 import { CustomProfile, Edge, FilterBuilder, GifBuilder, ImageChannels, NessBuilder } from "..";
+import { gifExtractor } from "../function"
 // import RankupBuilder from "../Managers/RankupBuilder";
 // import {canvasGif} from '../Managers/test';
 // const fs = require('fs');
 // const path = require('path');
 
 async function test() {
-  const background = await loadImage('https://media.discordapp.net/attachments/1006600590408818810/1006600665298116728/background-3147808.jpg');
+  // const background = await loadImage('https://media.discordapp.net/attachments/1006600590408818810/1006600665298116728/background-3147808.jpg');
   
-  const avatar = await loadImage('https://media.discordapp.net/attachments/758031322244710601/1000153437813616650/perso_anime_U565bW7EhY2InkF.png');
+  // const avatar = await loadImage('https://media.discordapp.net/attachments/758031322244710601/1000153437813616650/perso_anime_U565bW7EhY2InkF.png');
 
   // const rank = await loadImage('https://cdn.discordapp.com/attachments/1006600590408818810/1006600664929030255/background-2412739.jpg?size=4096')
 
-  const filter = new FilterBuilder(avatar)
-  await filter.Invert();
+  // const filter = new FilterBuilder(avatar)
+  // await filter.Invert();
+
+  
+  // const web = await loadImage("./assets/image/background/web-3876081.jpg")
+  const gif = "./assets/image/gif/stickMan.gif"
+  const gif2 = "./assets/image/gif/tvHS.gif"
+  const image = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg'
+
   
   const builder = new GifBuilder(700, 250)
   .setCornerRadius(15)
-  .setBackground(background)
-  .setFrame("Square", { x: 25, y:25 }, { width: 150, height: 150 }, { content: {imageOrText: filter.getCanvas()}})
-  .setFrame("Polygones", { x: 550, y:25 }, { width: 130, height: 130 }, {radPik: 6, content: { imageOrText: 33, textOptions: { font: "sans-serif", size: 80, color: "#000000", textAlign: "center", textBaseline: "middle" }}})
-  .setExp(false, {x: 45, y: 200}, {width: 655, height: 30}, 20, 65)
-  .setText('Hello World!', {x:350, y:100}, {size: 40, font: 'Impact'})
+  .setBackground(gif)
+  .setImage(gif2, {sx: 250, sy: 25, sWidth: 250, sHeight: 150})
+  .setFrame("Square", { x: 25, y: 25 }, { width: 150, height: 150 }, { content: {imageOrText: image}})
+  .setFrame("Circle", { x: 500, y: 50 }, { width: 150, height: 150 }, { content: {imageOrText: gif}})
+  .setFrame("Pentagone", { x: 300, y: 100 }, { width: 150, height: 150 }, { content: {imageOrText: gif2}})
+  // .setFrame("Polygones", { x: 550, y:25 }, { width: 130, height: 130 }, {radPik: 6, content: { imageOrText: 33, textOptions: { font: "sans-serif", size: 80, color: "#000000", textAlign: "center", textBaseline: "middle" }}})
+  // .setExp(false, {x: 45, y: 200}, {width: 655, height: 30}, 20, 65)
+  // .setText('Hello World!', {x:350, y:100}, {size: 40, font: 'Impact'})
 
-  await builder.test("https://thumbs.gfycat.com/GaseousHandmadeBurro-max-1mb.gif", avatar)
+  await builder.test()
+
+  // const background = await loadImage('https://media.discordapp.net/attachments/1006600590408818810/1006600665298116728/background-3147808.jpg');
+  // const avatar = await loadImage('https://media.discordapp.net/attachments/758031322244710601/1000153437813616650/perso_anime_U565bW7EhY2InkF.png');
+  
+
+  // const ness = new NessBuilder(700, 250) // Set Image format
+  // .setCornerRadius(15) // round the edges of the image
+  // .setBackground(background) // Add Background
+  // .setFrame("Square", { x: 25, y:25 }, { width: 150, height: 150 }, { radPik: 15, content: {imageOrText: avatar}}) // Add image in a square frame
+  // .setFrame("Polygones", { x: 550, y:25 }, { width: 130, height: 130 }, {radPik: 6, content: { imageOrText: image }}) // Write "33" in a polygones frame
+  // .setExp(false, {x: 45, y: 200}, {width: 655, height: 30}, 20, 65) // Draw an experience bar
+  // .setText('Hello World!', {x:350, y:100}, {size: 40, font: 'Impact'}) // Write "Hello World!"
+  // .generatedTo('.', "test", "png");
 
 
 
